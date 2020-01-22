8370521807cef7352ca875a8afa0d916a9f1d2e2
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-01-23 02:41:45 +0530

    structure vpc and bastion creation

--- VPC-Lambda-APIGateway/task.py
+++ VPC-Lambda-APIGateway/task.py
@@ -1,6 +1,7 @@
 import boto3
 
 ec2 = boto3.resource('ec2', region_name='us-east-1')
+ec2_client = boto3.client('ec2', region_name='us-east-1')
 
 # create VPC
 vpc = ec2.create_vpc(
@@ -14,58 +15,112 @@ vpc.create_tags(Tags=[
 ])
 vpc.wait_until_available()
 
-ec2_client = boto3.client('ec2', region_name='us-east-1')
 
-ec2Client.modify_vpc_attribute(
+ec2_client.modify_vpc_attribute(
     VpcId=vpc.id, 
     EnableDnsSupport={'Value': True}
 )
 
-ec2Client.modify_vpc_attribute(
+ec2_client.modify_vpc_attribute(
     VpcId=vpc.id,
     EnableDnsHostnames={'Value': True}
 )
 
-# create igw for internet access
+# create 2xPublic and 2xPrivate subnets 
+subnet_1_public = vpc.create_subnet(
+    CidrBlock='10.0.1.0/24',
+    AvailabilityZone='us-east-1a'
+)
+subnet_2_public = vpc.create_subnet(
+    CidrBlock='10.0.2.0/24',
+    AvailabilityZone='us-east-1b'
+)
+subnet_3_private = vpc.create_subnet(
+    CidrBlock='10.0.3.0/24',
+    AvailabilityZone='us-east-1a'
+)
+subnet_4_private = vpc.create_subnet(
+    CidrBlock='10.0.4.0/24',
+    AvailabilityZone='us-east-1b'
+)
+
+subnet_1_public.meta.client.modify_subnet_attribute(SubnetId=subnet_1_public.id, MapPublicIpOnLaunch={"Value": True})
+subnet_2_public.meta.client.modify_subnet_attribute(SubnetId=subnet_2_public.id, MapPublicIpOnLaunch={"Value": True})
+
+# create tags for the subnets
+subnet_1_public_tag = [subnet_1_public.id]
+subnet_2_public_tag = [subnet_2_public.id]
+subnet_3_private_tag = [subnet_3_private.id]
+subnet_4_private_tag = [subnet_4_private.id]
+
+ec2_client.create_tags(Resources=subnet_1_public_tag,Tags=[{'Key':'Name','Value':'boto3_public_subnet_1'}])
+ec2_client.create_tags(Resources=subnet_2_public_tag,Tags=[{'Key':'Name','Value':'boto3_public_subnet_2'}])
+ec2_client.create_tags(Resources=subnet_3_private_tag,Tags=[{'Key':'Name','Value':'boto3_private_subnet_1'}])
+ec2_client.create_tags(Resources=subnet_4_private_tag,Tags=[{'Key':'Name','Value':'boto3_private_subnet_2'}])
+
+
+# create igw for internet access and attach to vpc
 igw = ec2.create_internet_gateway()
-# print(igw.id)
 vpc.attach_internet_gateway(InternetGatewayId=igw.id)
 
 # create route table and add public route
 route_table = vpc.create_route_table()
 public_route = route_table.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=igw.id)
 
-# create subnets 
-subnet_1 = ec2_client.create_subnet(
-    CidrBlock='10.0.1.0/24',
-    VpcId=vpc.id,
-)
-subnet_2 = ec2_client.create_subnet(
-    CidrBlock='10.0.2.0/24',
-    VpcId=vpc.id,
-)
-subnet_3 = ec2_client.create_subnet(
-    CidrBlock='10.0.3.0/24',
-    VpcId=vpc.id,
-)
-subnet_4 = ec2_client.create_subnet(
-    CidrBlock='10.0.4.0/24',
-    VpcId=vpc.id,
-)
 
 # make subnet_1 and subnet_2 public and leave others private
-route_table.associate_with_subnet(SubnetId=subnet_1.get('Subnet').get('SubnetId'))
-route_table.associate_with_subnet(SubnetId=subnet_2.get('Subnet').get('SubnetId'))
+route_table.associate_with_subnet(SubnetId=subnet_1_public.id)
+route_table.associate_with_subnet(SubnetId=subnet_2_public.id)
+
+# Private route table for private subnets
+private_route_table = ec2.create_route_table(VpcId=vpc.id)
+private_route_table.associate_with_subnet(SubnetId=subnet_3_private.id)
+private_route_table.associate_with_subnet(SubnetId=subnet_4_private.id)
 
 # allocate elastic IPs
 allocation = ec2_client.allocate_address(
-    Domain='vpc'|'standard'
+    Domain='vpc'
 )
 
 # create NAT gateway
 nat_gw = ec2_client.create_nat_gateway(
-    AllocationId=allocation['AllocationId']
-    SubnetId=subnet_1['Subnet']['SubnetId']
+    AllocationId=allocation['AllocationId'],
+    SubnetId=subnet_1_public.id
+)
+nat_gw_id = nat_gw['NatGateway']['NatGatewayId']
+
+# attach NAT to private subnets
+ec2_client.create_route(RouteTableId=private_route_table.id,DestinationCidrBlock='0.0.0.0/0',NatGatewayId=nat_gw_id)
+
+# create security group for bastion host
+bastion_sec_group = ec2.create_security_group(
+    GroupName='ssh_bastion',
+    Description='allow ssh to bastion',
+    VpcId=vpc.id
+)
+
+bastion_sec_data_ingress = ec2_client.authorize_security_group_ingress(
+    GroupId=bastion_sec_group.id,
+    IpPermissions=[
+        {
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        }
+    ]
+)
+
+bastion_sec_data_egress = ec2_client.authorize_security_group_ingress(
+    GroupId=bastion_sec_group.id,
+    IpPermissions=[
+        {
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        }
+    ]
 )
 
 # create a security group for instances in private subnet
@@ -78,33 +133,17 @@ sec_group = ec2.create_security_group(
 sec_data = ec2_client.authorize_security_group_ingress(
     GroupId=sec_group.id,
     IpPermissions=[
-        {'IpProtocol': 'tcp',
-            'FromPort': 80,
-            'ToPort': 80,
-            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        {
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpRanges': [{'CidrIp': bastion_sec_group.id}]
         },
-        {'IpProtocol': 'tcp',
-            'FromPort': 22,
-            'ToPort': 22,
-            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-        }
-    ]
-)
-
-# create security group for bastion host
-bastion_sec_group = ec2.create_security_group(
-    GroupName='ssh_bastion',
-    Description='allow ssh to bastion',
-    VpcId=vpc.id
-)
-
-bastion_sec_data = ec2_client.authorize_security_group_ingress(
-    GroupId=bastion_sec_group.id,
-    IpPermissions=[
-        {'IpProtocol': 'tcp',
-            'FromPort': 22,
-            'ToPort': 22,
-            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        {
+            'CidrIp':'10.10.1.0/24',
+            'IpProtocol':'-1',
+            'FromPort':'-1',
+            'ToPort':'-1',
         }
     ]
 )
@@ -118,7 +157,8 @@ bastion_host = ec2.create_instances(
     SubnetId=subnet_1['Subnet']['SubnetId'],
     SecurityGroups=[
         bastion_sec_group.id,
-    ]
+    ],
+    KeyName='test-keyfile'
 )
 
 private_host = ec2.create_instances(
@@ -129,5 +169,6 @@ private_host = ec2.create_instances(
     SubnetId=subnet_3['Subnet']['SubnetId'],
     SecurityGroups=[
         sec_group.id,
-    ]
+    ],
+    KeyName='private_ssh'
 )
